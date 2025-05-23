4bc0eee54299f870dcd8cd05d447253a139fce08
khanosama783 <khanosama783@gmail.com>
2025-05-23 11:55:56 +0530

    Update URL patterns to route the root URL to the index view

--- DjangoMart/urls.py
+++ DjangoMart/urls.py
@@ -16,6 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from core.views import index
 
-
-urlpatterns = [path("admin/", admin.site.urls), path("", include("core.urls"))]
+urlpatterns = [path("", index)]
